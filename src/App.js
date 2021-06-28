a201b08bbb845efe0f3fef5918f80406781fc7a6
David Saah <dasorange.hope@gmail.com>
2021-06-28 13:45:06 +0000

    Succesfully allowed communication between spotify api and react app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
+import { useEffect, useState } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 import Login from './Components/Login';
+import Player from './Components/Player';
+import { useDataLayerValue } from './DataLayer';
+import { getTokenFromUrl } from './spotify';
 import './Styles/App.css';
 
+
+let spotify = new SpotifyWebApi()
+
 function App() {
+
+  const [token, setToken] = useState('')
+
+  // Grabing data from a data layer
+  const [{ user }, dispatch] = useDataLayerValue()
+
+  //Runs each time the page refreshes or there is a change in dependent values
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+    window.location.hash = ''
+    const _token = hash.access_token
+
+    if (_token) {
+      setToken(_token)
+
+      //setting the access token for the spotify API
+      spotify.setAccessToken(_token)
+      spotify.getMe().then(user => {
+        // dispatch add data to the data layer
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
+      })
+    }
+  }, [])
+
+  console.log('User data', user)
+
   return (
     <div className="app">
-      <Login />
+      {/* if logged in successfully render Player component else render Login component */}
+      {
+        token ? <Player user={user} /> : <Login />
+      }
     </div>
   );
 }
 
 export default App;
-
-
-
-//Client ID: 977caab9cac640b9b21998352a0498bf
-
