86763247269043e511307230020108dbeb0806d9
David Saah <dasorange.hope@gmail.com>
2021-07-06 14:37:42 +0000

    Component for displaying song lists from spotify api

---
+++ src/Components/SongRow.js
@@ -0,0 +1,21 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+import '../Styles/SongRow.css'
+
+function SongRow({ track }) {
+    console.log(track)
+
+    return (
+        <div className='songRow'>
+            <img className='songRow__album' src={track.album.images[0].url} alt='' />
+            <div className="songRow__info">
+                <Typography variant='body1'>{track.name}</Typography>
+                <Typography vaiant='body2' className='T_p'>
+                    {track.artists.map((artist) => artist.name).join(', ')} &nbsp;
+                    {track.album.name}
+                </Typography>
+            </div>
+        </div>
+    )
+}
+export default SongRow
