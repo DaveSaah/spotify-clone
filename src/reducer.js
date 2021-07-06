40573ec22d85c457a9d2eb376734469161c49d17
David Saah <dasorange.hope@gmail.com>
2021-07-06 15:00:48 +0000

    Script that uses UseContextAPI to store and data

--- src/reducer.js
+++ src/reducer.js
@@ -1,25 +1,44 @@
 export const initialState = {
-    user: null, 
-    playlists: [], 
-    playing: false, 
-    item: null, 
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    token: null,
+    discover_weekly: null,
 }
 
 const reducer = (state, action) => {
-    console.log(action)
+    // console.log(action)
 
-    //Action -> type, [payload]
-
-    switch(action.type) {
+    switch (action.type) {
 
         // SET_USER is the action, user is the payload
         case 'SET_USER':
             return {
-                ...state, 
+                ...state,
                 user: action.user
             }
-        default: 
-        return state
+
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
+            }
+
+        case 'SET_PLAYLISTS':
+            return {
+                ...state, 
+                playlists: action.playlists
+            }
+
+        case 'SET_DISCOVER_WEEKLY':
+            return {
+                ...state, 
+                discover_weekly: action.discover_weekly
+            }
+
+        default:
+            return state
     }
 }
 
